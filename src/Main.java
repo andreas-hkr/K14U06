b1c8a4dea6c0e5c315d2003c30d07f2e2ca382f0
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 09:25:52 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass Counter där man kan sätta ett intervall och sen öka/minska
+        // nuvarande värde genom metodanrop. Går man utanför intervallet skall en
+        // felsignal genereras.
+
+
+    }
+}
